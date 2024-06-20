9157cc0c00cc8bacfdefac697cdd9f3581f3517f
Abram Bradley <abramabradley@gmail.com>
2024-06-20 11:16:12 -0400

    Added boiling lab

---
+++ labs/boiling-water/boiling.cpp
@@ -0,0 +1,35 @@
+/// PARTICIPATION GRADE
+
+//  - auto-format in vim: gg=G in normal mode, in vscode: alt+shift+f
+//  - gradescript command: python3.11 scripts/test.py boiling.cpp
+
+#include <cmath> // new: for pow and log
+#include <fstream>
+#include <iostream>
+#include <sstream> // new: for istringstream
+#include <string>
+#include <vector>
+
+using namespace std;
+
+double elevationToBoilingPoint(const double elevation) {
+    double boilingPoint = 0.0;
+
+    // TODO Calculate the boiling point of water at the given elevation
+    //      using the equation provided in the lab description.
+
+    return boilingPoint;
+}
+
+int main(int argc, char **argv) {
+
+    // TODO
+    // - Use argv and argc to get a filename. Error check
+    // - Read the file, error check, and store the city names and elevations in
+    //   vectors (you can discard the state names)
+    // - Ask for which city
+    // - Calculate the boiling point at that city's elevation using the given
+    //   formulas
+
+    return 0;
+}
