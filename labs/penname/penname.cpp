9db29fa8171410768e8a8f96769415b92e20f966
Silverasdf <ryanandmichelle2003@gmail.com>
2024-08-23 11:49:34 -0400

    added penname

---
+++ labs/penname/penname.cpp
@@ -0,0 +1,28 @@
+/* Program Name: PROGRAM NAME HERE
+ * Student Name: YOUR NAME HERE
+ * Net ID: NETID HERE
+ * Student ID: STUDENT ID HERE (000-12-3456)
+ * Program Description: BRIEF, 1-2 SENTENCE DESCRIPTION HERE
+ * Sources: Cite any instructors, students, or external resources you used here.
+ *   - Do not directly copy code from any source:
+ *     other students, Stack Overflow, ChatGPT, etc.
+ *   - Do not include code in your solution you do not understand or cannot
+ *     explain.
+ *   - We check plagiarism for similarities and irregularities.
+ */
+
+//! Remember: your comments
+//! Remember: your formatting and indentation
+//  - auto-format in vim with gg=G in normal mode, in vscode with alt+shift+f
+//! Remember: check your solution with the gradescripts
+//  - gradescript command: python3.11 scripts/test.py penname.cpp
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    // TODO
+    return 0;
+}
