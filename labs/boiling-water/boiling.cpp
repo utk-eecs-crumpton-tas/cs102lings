69535b59c5001247268e216fa06ae563ff08d209
Silverasdf <ryanandmichelle2003@gmail.com>
2024-09-19 13:04:27 -0400

    added boiling point f24

---
+++ labs/boiling-water/boiling.cpp
@@ -0,0 +1,49 @@
+/* Program Name: PROGRAM NAME HERE
+ * Student Name: YOUR NAME HERE
+ * Net ID: NETID HERE
+ * Student ID: STUDENT ID HERE (000-12-3456)
+ * Program Description: BRIEF, 1-2 SENTENCE DESCRIPTION HERE
+ * Sources: Cite any instructors, students, or external resources you used here.
+ *   - Do not directly copy code from any source:
+ *     other students, Stack Overflow, ChatGPT, etc.
+ *   - Do not include code in your solution you do not understand or cannot
+ *     explain.
+ *   - We check plagiarism for similarities and irregularities.
+ */
+
+//! Remember: your comments
+//! Remember: your formatting and indentation
+//  - auto-format in vim with gg=G in normal mode, in vscode with alt+shift+f
+//! Remember: check your solution with the gradescripts
+//  - gradescript command: python3.11 scripts/test.py boiling.cpp
+
+#include <cmath> // new: for pow and log
+#include <fstream>
+#include <iostream>
+#include <sstream> // new: for istringstream
+#include <string>
+#include <vector>
+
+using namespace std;
+
+double elevationToBoilingPoint(const double elevation) {
+    double boilingPoint = 0.0;
+
+    // TODO Calculate the boiling point of water at the given elevation
+    //      using the equation provided in the lab description.
+
+    return boilingPoint;
+}
+
+int main(int argc, char **argv) {
+
+    // TODO
+    // - Use argv and argc to get a filename. Error check
+    // - Read the file, error check, and store the city names and elevations in
+    //   vectors (you can discard the state names)
+    // - Ask for which city
+    // - Calculate the boiling point at that city's elevation using the given
+    //   formulas
+
+    return 0;
+}
